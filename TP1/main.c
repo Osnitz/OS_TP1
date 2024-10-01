9ce513e1e7539e65fd0dc741c9bad23f5498e93e
osnitz <lolmatt18@gmail.com>
2024-10-01 08:41:48 +0200

    Initial commit

---
+++ TP1/main.c
@@ -0,0 +1,9 @@
+//
+// Created by matthieu on 01/10/24.
+//
+#include <stdio.h>
+#include "main.h"
+
+int main() {
+    printf("Hello, World!\n");;
+}
---
+++ TP1/main.h
@@ -0,0 +1,8 @@
+//
+// Created by matthieu on 01/10/24.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#endif //MAIN_H
